2060f898759d98d32aececf858561c99a112818b
nassos_from_ubuntu <nassosbountioukos@gmail.com>
2022-01-21 21:44:13 +0200

    pushed from ubuntu

--- python_script.py
+++ python_script.py
@@ -5,7 +5,7 @@ import sys
 import csv
 import math
 
-from energy import get_energy
+#from energy2 import getEnergy
 
 folder_path = ""#sys.argv[1]
 
@@ -67,8 +67,8 @@ def get_e_d(folder, info_dict= {}):
    #         pass
         os.system(f"python GEM5ToMcPAT.py {folder}/{filename}/stats.txt {folder}/{filename}/config.json inorder_arm.xml -o made_xmls/{filename}")
         os.system(f"../mcpat/mcpat -infile made_xmls/{filename} -print_level 5 > made_prints/{filename}")
-        # os.system(f"python print_energy.py made_prints/{filename} folder/{filename}/stats.txt")
-        info_dict[d]["energy"], info_dict[d]["leakage"], info_dict[d]["dynamic"], info_dict[d]["total"], info_dict[d]["runtime"], info_dict[d]["area"] = get_energy(f"made_prints/{filename}", f"{folder}/{filename}/stats.txt" )
+        os.system(f"python print_energy.py made_prints/{filename} {folder}/{filename}/stats.txt > energy_info/{filename}.txt")
+        #info_dict[d]["energy"], info_dict[d]["leakage"], info_dict[d]["dynamic"], info_dict[d]["total"], info_dict[d]["runtime"], info_dict[d]["area"] = get_energy(f"made_prints/{filename}", f"{folder}/{filename}/stats.txt" )
     return info_dict
 def dict_to_csv(dict, filename):
     header = "formula,energy,area,delay"
